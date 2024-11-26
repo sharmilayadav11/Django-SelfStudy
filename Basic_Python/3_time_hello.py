5e5e797e542d5441ff149ff0639d9da5ee1ebc5e
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-11-26 23:41:56 +0200

    df

---
+++ Basic_Python/3_time_hello.py
@@ -0,0 +1,8 @@
+# Create a function named tripleprint  that takes a string as a parameter and prints that string 3 times in a row. So if I passed in the string hello  it would print hellohellohello
+
+def tripleprint():
+    word = 'sharmila'
+    print(word*3)
+
+
+tripleprint()
---
+++ Basic_Python/dictionary.py
@@ -0,0 +1,9 @@
+words = ["PoGo", "Spange", "Lie-Fi"]
+
+definitions = ["Slang for Pokemon Go",
+               "To collect spare  change, either from couches, passerbys on the street or any numerous other ways and means",
+               "When your phone or tablet indicates that you are connected to a wireless network, however you are still unable to load webpages or use any internet services with your device"]
+
+cooldictionary = {}
+for i in range(0, 3):
+    cooldictionary[words[i]] = definitions[i]
